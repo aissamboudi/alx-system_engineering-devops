1baca50663b55ea5c6e2983ba50a6dcfbdc6581c
aissamboudi <aissamboudi@gmail.com>
2024-08-14 00:38:09 +0100

    new update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""contains the definiton of number_of_subscribers function"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
     if response.ok:
         return response.json().get('data').get('subscribers')
     else:
